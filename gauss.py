a9d28c580b07f470a75b71372e273d14ad1de8cf
Randall Woodall <randalljwoodall@gmail.com>
2020-03-15 17:16:55 -0600

    Made a power flow solving script.

---
+++ gauss.py
@@ -0,0 +1,80 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Randall Woodall
+# March 15, 2020
+# gauss.py
+# Perform Gauss powerflow calculations given an admittance matrix.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+import numpy as np
+from admittance import Admittance
+
+
+class Gauss:
+    def __init__(self, admittance, P, Q, V, d):
+        self.admittance = admittance
+        self.origin_P = P
+        self.origin_Q = Q
+        self.origin_V = V
+        self.origin_d = d
+        self.S = []
+        for i in range(len(P)):
+            if P[i] == -1:
+                P1 = 1
+            else:
+                P1 = P[i]
+            if Q[i] == -1:
+                Q1 = 1
+            else:
+                Q1 = Q[i]
+            self.S.append(np.complex(P1, Q1))
+        self.V = []
+        for i in range(len(V)):
+            if V[i] == -1:
+                self.V.append(1)
+            else:
+                self.V.append(V[i])
+
+    def solve(self):
+        V_old = self.V
+        self.V = self.iterate_V()
+
+        while np.linalg.norm(np.array(V_old) - np.array(self.V)) > .1:
+            # Replace known
+            for i in range(len(self.origin_V)):
+                if self.origin_V[i] != -1:
+                    self.V[i] = self.origin_V[i]
+            self.S = self.iterate_S()
+            # Replace known
+            for i in range(len(self.origin_P)):
+                if self.origin_P[i] == -1:
+                    P1 = np.real(self.S[i])
+                else:
+                    P1 = self.origin_P[i]
+                if self.origin_Q[i] == -1:
+                    Q1 = np.imag(self.S[i])
+                else:
+                    Q1 = self.origin_Q[i]
+                self.S[i] = np.complex(P1, Q1)
+            V_old = self.V
+            self.V = self.iterate_V()
+            print(self.V)
+            print(self.S)
+
+    def iterate_V(self):
+        new_V = []
+        for i in range(len(self.V)):
+            sum = 0
+            for j in range(len(self.admittance[0])):
+                sum += self.admittance[i][j] * self.V[j]
+            new_V.append(1 / self.admittance[i][i] * ((np.conj(self.S[i]/self.V[i]) - sum) + self.admittance[i][i] *
+                                                      self.V[i]))
+        return new_V
+
+    def iterate_S(self):
+        new_S = []
+        for i in range(len(self.S)):
+            sum = 0
+            for j in range(len(self.admittance[0])):
+                sum += self.V[j] * self.admittance[i][j]
+            new_S.append(self.V[i] * np.conj(sum))
+        return new_S
--- main.py
+++ main.py
@@ -6,14 +6,75 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 from admittance import Admittance
-from newtonRaphson import NewtonRaphson
+from gauss import Gauss
 
 import sys
 import numpy as np
+import wx
+import pandas as pd
 
-test1 = Admittance(open(sys.argv[1], 'r+').readlines())
-powerFlow = NewtonRaphson(test1)
+# Globals
+admittance = None
+powerFlow = None
 
-np.set_printoptions(linewidth=5000)
-print(test1.admittance)
 
+def loadAdmittance(fName):
+    frm.SetStatusText('Building admittance matrix...')
+    return Admittance(open(fName, 'r+').readlines())
+
+
+def Quit(e):
+    frm.Close()
+
+
+def Load(e):
+    global admittance
+    with wx.FileDialog(frm, 'Open Line Information') as fileDialog:
+        if fileDialog.ShowModal() == wx.ID_CANCEL:
+            return
+        fname = fileDialog.GetPath()
+
+    admittance = loadAdmittance(fname)
+    wx.StaticText(frm, 1, 'Y = ' + np.array2string(admittance.admittance))#  +
+                  #'\n\nZ = ' + np.array2string(np.linalg.inv(admittance.admittance)))
+    frm.SetStatusText('')
+
+def Run(e):
+    global powerFlow
+    if admittance == None:
+        return
+    # Else
+    frm.SetStatusText('Running Gauss-Seidel Power Flow')
+    # Takes admittance matrix and known values at each bus
+    with wx.FileDialog(frm, 'PQVd Information') as fileDialog:
+        if fileDialog.ShowModal() == wx.ID_CANCEL:
+            return
+        fname = fileDialog.GetPath()
+    pqvd = pd.read_csv(fname, header=None)
+    powerFlow = Gauss(admittance.admittance, pqvd[0].values, pqvd[1].values, pqvd[2].values, pqvd[3].values)
+    powerFlow.solve()
+    frm.SetStatusText('')
+
+
+if __name__ == '__main__':
+    app = wx.App()
+    frm = wx.Frame(None, title='Power Flow Program', size=(1200, 800))
+    frm.SetBackgroundColour(wx.Colour(wx.WHITE))
+    frm.Show()
+    frm.CreateStatusBar()
+    menubar = wx.MenuBar()
+    fileMenu = wx.Menu()
+    menubar.Append(fileMenu, '&File')
+    load_admittance_button = fileMenu.Append(wx.ID_FILE, 'Load Lines', 'Load Lines into Admittance')
+    gauss_run_button = fileMenu.Append(wx.ID_FILE1, 'Run Gauss', 'Runs Gauss Power Flow on Admittance Matrix')
+    exit_button = fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit Application')
+    frm.Bind(wx.EVT_MENU, Quit, exit_button)
+    frm.Bind(wx.EVT_MENU, Load, load_admittance_button)
+    frm.Bind(wx.EVT_MENU, Run, gauss_run_button)
+    frm.SetMenuBar(menubar)
+
+    frm.SetStatusText('')
+
+    np.set_printoptions(linewidth=5000)
+
+    app.MainLoop()
